1cc5890196ac64591c4ba49231b38854a956a83c
Robert Biggs <rbiggs@ymail.com>
2018-11-25 19:49:54 -0800

    Importing actions from separate file.

--- tour-of-heroes tagged-union/src/js/components/app.js
+++ tour-of-heroes tagged-union/src/js/components/app.js
@@ -5,7 +5,7 @@ import { HeroList } from './hero-list'
 import HeroDetail from './hero-detail'
 import fetchHeroes from '../utils/fetch-heroes'
 import { Msg } from '../utils/tagged-union'
-
+import { actions } from '../utils/actions'
 
 
 function getHeroes(send) {
@@ -49,57 +49,6 @@ const state = {
   newId: 21,
   newHero: '',
   inputValue: ''
-}
-
-function actions(msg, prevState) {
-  return Msg.match(msg, {
-    'UseFetchedHeroes': heroes => {
-      prevState.heroes = heroes
-      return [prevState]
-    }, 
-    'ActiveComponent': component => { 
-      prevState.activeComponent = component
-      return [prevState]
-    }, 
-    'ShowDetail': id => {
-      const position = prevState.heroes.findIndex(person => person.id == id)
-      const hero = prevState.heroes[position]
-      try {
-        hero.originalName = hero.name
-        prevState.activeComponent = 'detail'
-        prevState.selectedHero = hero
-      } catch (err) { }
-      return [prevState]
-    }, 
-    'DeleteItem': id => {
-      prevState.heroes = prevState.heroes.filter(hero => hero.id != id)
-      return [prevState]
-    }, 
-    'ChangeHeroName': name => {
-      prevState.selectedHero.name = name
-      return [prevState]
-    }, 
-    'ResetName': () => { 
-      prevState.selectedHero.name = prevState.selectedHero.originalName
-      return [prevState]
-    }, 
-    'SaveName': () => { 
-      window.location.hash = '#/heroes'
-      return [prevState]
-    }, 
-    'NewHero': newHero => { 
-      prevState.newHero = newHero
-      return [prevState]
-    }, 
-    'AddHero': () => {
-      prevState.heroes.push({
-        id: prevState.newId++,
-        name: prevState.newHero
-      })
-      prevState.newHero = '' 
-      return [prevState]
-    }
-  })
 }
 
 export const program = {
--- tour-of-heroes tagged-union/src/js/utils/tagged-union.js
+++ tour-of-heroes tagged-union/src/js/utils/tagged-union.js
@@ -1,2 +1,3 @@
 import { union } from '@composi/core'
+
 export const Msg = union(['UseFetchedHeroes', 'ActiveComponent', 'ShowDetail', 'DeleteItem', 'ChangeHeroName', 'ResetName', 'SaveName', 'NewHero', 'AddHero'])
