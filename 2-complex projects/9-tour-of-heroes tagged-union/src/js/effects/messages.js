7b5be1612bc3dcbcbaf47095202bc47717dee072
Robert Biggs <rbiggs@ymail.com>
2019-11-20 07:37:28 -0800

    Formatting for Tour of Heroes messages.

--- 2-complex projects/9-tour-of-heroes tagged-union/src/js/effects/messages.js
+++ 2-complex projects/9-tour-of-heroes tagged-union/src/js/effects/messages.js
@@ -1,6 +1,32 @@
 import { union } from '@composi/core'
 
 /** @type {import('../types').MessageUnion} */
-export const Msg = union('ActiveComponent', 'AddHero', 'ChangeHeroName', 'DeleteHero', 'NewHero', 'ResetName', 'ResetSearchResults', 'SaveLocally', 'SaveName', 'Search', 'ShowDetail', 'UseFetchedHeroes')
+export const Msg = union(
+  'ActiveComponent',
+  'AddHero',
+  'ChangeHeroName',
+  'DeleteHero',
+  'NewHero',
+  'ResetName',
+  'ResetSearchResults',
+  'SaveLocally',
+  'SaveName',
+  'Search',
+  'ShowDetail',
+  'UseFetchedHeroes'
+)
 
-export const { ActiveComponent, AddHero, ChangeHeroName, DeleteHero, NewHero, ResetName, ResetSearchResults, SaveLocally, SaveName, Search, ShowDetail, UseFetchedHeroes } = Msg
+export const {
+  ActiveComponent,
+  AddHero,
+  ChangeHeroName,
+  DeleteHero,
+  NewHero,
+  ResetName,
+  ResetSearchResults,
+  SaveLocally,
+  SaveName,
+  Search,
+  ShowDetail,
+  UseFetchedHeroes
+} = Msg
